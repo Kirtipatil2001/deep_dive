cb3489308023a21d630fb3e1da757d540c3463dd
Kirti Ravindra Patil <79988064+Kirtipatil2001@users.noreply.github.com>
2024-12-03 14:18:35 +0530

    Use of ng Projecton

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-button', //[] is is called and used for property binding in that bracket we can declare an attribute
+  selector: 'button[appButton], a[appButton]', //[] is is called and used for property binding in that bracket we can declare an attribute
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
