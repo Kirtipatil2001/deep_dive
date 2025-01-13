a2056c7d5544593bb7080166697a3b1584e6da47
Kirti Ravindra Patil <79988064+Kirtipatil2001@users.noreply.github.com>
2025-01-13 21:50:48 +0530

    closer look
    cleanup functions
    Type models
    repitition

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-server-status',
@@ -7,7 +8,44 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus!: 'online';
+export class ServerStatusComponent  implements OnInit {
+  currentStatus = signal< 'online' |  'offline' |  'unknown'>('offline');
+  //private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef);
 
+  constructor() {
+    effect(() => {
+      console.log(this.currentStatus());
+    });
+
+  }
+
+ngOnInit(){
+    console.log('ON INIT');
+    const interval = setInterval(() => {
+      const rnd = Math.random(); //produce value between o and 0.99999999 == 0-->0.999999999999999999
+
+      if(rnd <  0.5){
+        this.currentStatus.set('online');
+      }
+      else if (rnd < 0.9){
+        this.currentStatus.set('offline');
+      }
+      else {
+        this.currentStatus.set('unknown');
+      }
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
+  }
+
+  ngAfterViewInit(){
+    console.log('AFTER VIEW INIT');
+  }
+
+  // ngOnDestroy(){
+  //   clearTimeout(this.interval);
+  // }
 }
--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent ,FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit, AfterViewInit{
+  @ViewChild('form') form?:ElementRef<HTMLFormElement>;
+  //private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  //@Output() add = new EventEmitter({title:string; text:string});
+  add = output<{title:string; text:string}>();
 
+  ngOnInit() { 
+    console.log('ONINIT ');
+    console.log(this.form?.nativeElement);
+  }
+
+  ngAfterViewInit(){
+    console.log('AFTER VIEW INIT ');
+    console.log(this.form?.nativeElement);
+  }
+
+  onSubmit(title: string, ticketText:string){
+    this.add.emit({title:title, text:ticketText});
+
+    this.form?.nativeElement.reset();
+
+  
+  
+    //console.log(titlElement);
+  //const enteredTitle = titlElement.value;
+  //console.log('ENTERED TITLE:' + enteredTitle);
+
+
+  }
 }
